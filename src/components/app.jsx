4eb7a43f5f225c01670a74220380b352d83640f5
takutoki <34600200+takutoki@users.noreply.github.com>
2018-05-30 16:39:37 +0900

    Introduce axios and geocoding api

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import SeachForm from './SearchForm';
 import GeocodeResult from './GeocodeResult';
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 class App extends Component {
   constructor(props) {
@@ -9,8 +12,43 @@ class App extends Component {
     };
   }
 
+  setErrorMessage(message) {
+    this.setState({
+      address: message,
+      lat: 0,
+      lng: 0,
+    });
+  }
+
   handlePlaceSubmit(place) {
-    console.log(place);
+    axios
+      .get(GEOCODE_ENDPOINT, { params: { address: place } })
+      .then((results) => {
+        console.log(results);
+        const data = results.data;
+        const result = results.data.results[0];
+        switch (data.status) {
+          case 'OK': {
+            const location = result.geometry.location;
+            this.setState({
+              address: result.formatted_address,
+              lat: location.lat,
+              lng: location.lng,
+            });
+            break;
+          }
+          case 'ZERO_RESULTS': {
+            this.setErrorMessage('結果が見つかりませんでした');
+            break;
+          }
+          default: {
+            this.setErrorMessage('エラーが発生しました');
+          }
+        }
+      })
+      .catch(() => {
+        this.setErrorMessage('エラーが発生しました');
+      });
   }
 
   render() {
@@ -19,9 +57,9 @@ class App extends Component {
         <h1>緯度経度検索</h1>
         <SeachForm onSubmit={place => this.handlePlaceSubmit(place)} />
         <GeocodeResult
-           address={this.state.address}
-           lat={this.state.lat}
-           lng={this.state.lng}
+          address={this.state.address}
+          lat={this.state.lat}
+          lng={this.state.lng}
         />
       </div>
     );
