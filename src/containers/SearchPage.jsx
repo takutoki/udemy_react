9ca2dedd9e37e751862370a619a017d4cf7b2212
takutoki <34600200+takutoki@users.noreply.github.com>
2018-06-05 16:02:40 +0900

    Move SearchPage.jsx to containers

--- src/containers/SearchPage.jsx
+++ src/containers/SearchPage.jsx
@@ -27,11 +27,18 @@ class SearchPage extends Component {
   }
 
   componentDidMount() {
-    const place = this.getPlacePrams();
-    // console.log(`componemntDidMount${place}`);
-    if (place) {
-      this.startSearch();
-    }
+    this.unsubscribe = this.props.store.subscribe(() => {
+      this.forceUpdate();
+    });
+    // const place = this.getPlacePrams();
+    // // console.log(`componemntDidMount${place}`);
+    // if (place) {
+    //   this.startSearch();
+    // }
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
   }
 
   getPlacePrams() {
@@ -56,7 +63,7 @@ class SearchPage extends Component {
 
   handlePlaceChange(e) {
     e.preventDefault();
-    this.props.onPlaceChange(e.target.value);
+    this.props.store.dispatch({ type: 'CHANGE_PLACE', place: e.target.value });
   }
 
   handlePlaceSubmit(e) {
@@ -100,11 +107,12 @@ class SearchPage extends Component {
   }
 
   render() {
+    const state = this.props.store.getState();
     return (
       <div className="search-page">
         <h1 className="app-title">ホテル検索</h1>
         <SearchForm
-          place={this.props.place}
+          place={state.place}
           onPlaceChange={e => this.handlePlaceChange(e)}
           onSubmit={e => this.handlePlaceSubmit(e)}
         />
@@ -133,8 +141,11 @@ class SearchPage extends Component {
 SearchPage.propTypes = {
   history: PropTypes.shape({ push: PropTypes.func }).isRequired,
   location: PropTypes.shape({ search: PropTypes.string }).isRequired,
-  onPlaceChange: PropTypes.func.isRequired,
-  place: PropTypes.string.isRequired,
+  store: PropTypes.shape({
+    subscribe: PropTypes.func,
+    getState: PropTypes.func,
+    dispatch: PropTypes.func,
+  }).isRequired,
 };
 
 export default SearchPage;
